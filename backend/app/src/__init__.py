315bae7c279026586c187010874de6bd203e0316
Ícaro <icaroribeiro@hotmail.com>
2024-04-14 15:27:32 -0300

    Add api exception handler

--- backend/app/src/__init__.py
+++ backend/app/src/__init__.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from src.controller import blueprint as endpoints
 
 
 def create_app() -> Flask:
     app = Flask(__name__)
+    app.config["RESTX_MASK_SWAGGER"] = False
+    app.config["ERROR_INCLUDE_MESSAGE"] = False
+    app.register_blueprint(blueprint=endpoints)
     return app
---
+++ backend/app/src/utils/api_exceptions.py
@@ -0,0 +1,24 @@
+from http import HTTPStatus
+
+from src.utils.json_response import respond_with_json
+from werkzeug.exceptions import HTTPException
+
+
+class ApiException(HTTPException):
+    def __init__(self, message: str, extra: str, code: int):
+        super().__init__()
+        rv = {"message": message}
+        if extra:
+            rv["extra"] = extra
+        self.data = rv
+        self.code = code
+
+
+class ServerErrorException(ApiException):
+    def __init__(
+        self,
+        message: str = "Oops! Something went wrong",
+        extra: str = None,
+        code: int = HTTPStatus.INTERNAL_SERVER_ERROR,
+    ):
+        super().__init__(extra=extra, message=message, code=code)

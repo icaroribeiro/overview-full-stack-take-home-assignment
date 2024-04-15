62af79e2ac594123c889c2ec2f7b15ca21818ce1
Ícaro <icaroribeiro@hotmail.com>
2024-04-15 18:24:23 -0300

    Add detections routes

--- backend/app/src/__init__.py
+++ backend/app/src/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from src.application_container import AppContainer, Core
 from src.controller import blueprint as endpoints
+from src.controller.routes import detections as detections_module
 from src.controller.routes import health_check as health_check_module
 from src.controller.routes import images as images_module
 from src.controller.routes import models as models_module
@@ -9,12 +10,14 @@ from src.infrastructure.database import get_database_url
 
 def create_app() -> Flask:
     database_url = get_database_url()
+
     Core.config.override({"database_url": database_url})
 
     container = AppContainer()
     container.wire(modules=[health_check_module])
     container.wire(modules=[models_module])
     container.wire(modules=[images_module])
+    container.wire(modules=[detections_module])
 
     app = Flask(__name__)
     app.config["RESTX_MASK_SWAGGER"] = False

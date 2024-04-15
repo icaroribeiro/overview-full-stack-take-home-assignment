0fa2c5eca10ce6e84be6926797656ca3ed607913
Ícaro <icaroribeiro@hotmail.com>
2024-04-15 15:49:19 -0300

    Update respond_with_json method from utils

--- backend/app/src/utils/json_response.py
+++ backend/app/src/utils/json_response.py
@@ -4,5 +4,5 @@ from flask import Response, make_response
 from pydantic import BaseModel
 
 
-def respond_with_json(payload: BaseModel, status_code: int) -> Response:
-    return make_response(json.loads(payload.model_dump_json()), status_code)
+def respond_with_json(payload: BaseModel, code: int) -> Response:
+    return make_response(json.loads(payload.model_dump_json()), code)

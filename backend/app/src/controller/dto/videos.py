cf69f056a8257f7a6b8c1e6a5c642bbb31097850
Ícaro <icaroribeiro@hotmail.com>
2024-04-18 09:44:35 -0300

    Add video data transfer objects and routes to controller layer

---
+++ backend/app/src/controller/dto/videos.py
@@ -0,0 +1,38 @@
+from datetime import datetime
+from typing import Optional
+
+from pydantic import BaseModel
+from src.domain.model.video import Video
+
+
+class VideoResponse(BaseModel):
+    id: str
+    name: str
+    image_path_list: list[str]
+    created_at: datetime
+
+    @staticmethod
+    def from_domain(video: Video) -> "VideoResponse":
+        image_path_list = (
+            [str(image_path) for image_path in video.image_path_list]
+            if video.image_path_list
+            else []
+        )
+        return VideoResponse(
+            id=str(video.id),
+            name=video.name,
+            image_path_list=image_path_list,
+            created_at=video.created_at,
+        )
+
+
+class CreateVideoResponse(VideoResponse):
+    pass
+
+
+class GetVideoResponse(VideoResponse):
+    pass
+
+
+class UploadImageResponse(BaseModel):
+    path: str
---
+++ backend/app/src/controller/routes/videos.py
@@ -0,0 +1,173 @@
+import json
+from http import HTTPStatus
+
+from dependency_injector.wiring import Provide, inject
+from flask_restx import Namespace, Resource, fields, reqparse
+from src import AppContainer
+from src.controller.dto.videos import (
+    CreateVideoResponse,
+    GetVideoResponse,
+    UploadImageResponse,
+)
+from src.infrastructure import application_settings
+from src.service.file import FileService
+from src.service.video import VideoService
+from src.utils.api_exceptions import NoContentException
+from src.utils.json_response import respond_with_json
+from werkzeug.datastructures import FileStorage
+
+api = Namespace(name="videos", description="Videos related operations")
+
+create_video_parser = reqparse.RequestParser()
+create_video_parser.add_argument("name", location="json", type=str, required=True)
+
+get_videos_parser = reqparse.RequestParser()
+get_videos_parser.add_argument(
+    "name",
+    location="args",
+    type=str,
+    required=False,
+)
+
+upload_image_parser = reqparse.RequestParser()
+upload_image_parser.add_argument(
+    "file", location="files", type=FileStorage, required=True
+)
+
+create_video_response_fields = api.model(
+    name="CreateVideoResponse",
+    model={
+        "id": fields.String(),
+        "name": fields.String(),
+        "image_path_list": fields.List(fields.String()),
+        "created_at": fields.Date(),
+    },
+)
+create_video_error_response_fields = api.model(
+    "CreateVideoErrorResponse",
+    {"message": fields.String(), "extra": fields.String()},
+)
+
+get_video_response_fields = api.model(
+    name="GetVideoResponse",
+    model={
+        "id": fields.String(),
+        "name": fields.String(),
+        "image_path_list": fields.List(fields.String()),
+        "created_at": fields.Date(),
+    },
+)
+get_videos_response_fields = [get_video_response_fields]
+get_videos_error_response_fields = api.model(
+    "GetVideosErrorResponse",
+    {"message": fields.String(), "extra": fields.String()},
+)
+
+upload_image_response_fields = api.model(
+    "UploadImageResponse", {"path": fields.String()}
+)
+upload_image_error_response_fields = api.model(
+    "UploadImageErrorResponse", {"message": fields.String(), "extra": fields.String()}
+)
+
+
+@api.route("/<string:video_id>/upload-image")
+class UploadImage(Resource):
+    @inject
+    def __init__(
+        self,
+        api=None,
+        file_service: FileService = Provide[AppContainer.service.file_service],
+        video_service: VideoService = Provide[AppContainer.service.video_service],
+        *args,
+        **kwargs,
+    ):
+        super().__init__(api, *args, **kwargs)
+        self.file_service = file_service
+        self.video_service = video_service
+
+    @api.response(code=200, model=upload_image_response_fields, description="OK")
+    def post(self, video_id: str):
+        args = upload_image_parser.parse_args()
+
+        if "file" not in args:
+            raise NoContentException(
+                extra="The request performed without the file part"
+            )
+
+        file = args["file"]
+
+        allowed_extensions = application_settings.allowed_image_extensions
+
+        image_path = self.file_service.save_file(
+            file=file, allowed_extensions=allowed_extensions
+        )
+
+        self.video_service.renew_image_path_list(id=video_id, image_path=image_path)
+
+        return respond_with_json(
+            payload=UploadImageResponse(path=image_path).json(),
+            code=HTTPStatus.OK.value,
+        )
+
+
+@api.route("")
+class Videos(Resource):
+    @inject
+    def __init__(
+        self,
+        api=None,
+        video_service: VideoService = Provide[AppContainer.service.video_service],
+        *args,
+        **kwargs,
+    ):
+        super().__init__(api, *args, **kwargs)
+        self.video_service = video_service
+
+    @api.doc("create_video")
+    @api.expect(create_video_parser)
+    @api.response(
+        code=HTTPStatus.CREATED.value,
+        model=create_video_response_fields,
+        description="Created",
+    )
+    @api.response(
+        code=HTTPStatus.INTERNAL_SERVER_ERROR.value,
+        model=create_video_error_response_fields,
+        description="Internal Server Error",
+    )
+    def post(self):
+        args = create_video_parser.parse_args()
+        name = args.get("name")
+
+        video = self.video_service.save_video(name)
+
+        return respond_with_json(
+            payload=CreateVideoResponse.from_domain(video=video).json(),
+            code=HTTPStatus.CREATED.value,
+        )
+
+    @api.doc("get_videos")
+    @api.expect(get_videos_parser)
+    @api.response(
+        code=HTTPStatus.OK.value,
+        model=get_videos_response_fields,
+        description="OK",
+    )
+    @api.response(
+        code=HTTPStatus.INTERNAL_SERVER_ERROR.value,
+        model=get_videos_error_response_fields,
+        description="Internal Server Error",
+    )
+    def get(self):
+        args = get_videos_parser.parse_args()
+        name = args.get("name")
+
+        video_list = self.video_service.retrieve_videos(name=name)
+
+        list_videos_response = [
+            json.loads(GetVideoResponse.from_domain(video=video).json())
+            for video in video_list
+        ]
+
+        return respond_with_json(payload=list_videos_response, code=HTTPStatus.OK.value)

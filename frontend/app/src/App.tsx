9be6103d376898baab96c86bc37e28abf1119684
Ícaro <icaroribeiro@hotmail.com>
2024-04-22 17:37:05 -0300

    Add App.tsx file

---
+++ frontend/app/src/App.tsx
@@ -0,0 +1,25 @@
+import NavigationBar from "./components/NavigationBar";
+import About from "./components/About";
+import Home from "./components/Home";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import ShowPredictions from "./pages/ShowPredictions";
+import MakePrediction from "./pages/MakePrediction";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <NavigationBar></NavigationBar>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/make-prediction" element={<MakePrediction />} />
+          <Route path="/show-predictions" element={<ShowPredictions />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;

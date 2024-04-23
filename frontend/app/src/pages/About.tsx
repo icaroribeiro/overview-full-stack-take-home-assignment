bf5e8d335b5656b2681919d51cac88b4aab7d5bd
Ícaro <icaroribeiro@hotmail.com>
2024-04-22 21:43:05 -0300

    Update About page content

--- frontend/app/src/pages/About.tsx
+++ frontend/app/src/pages/About.tsx
@@ -2,7 +2,9 @@ const About = () => {
   return (
     <div>
       <b>Contact Info</b>
+      <br></br>
       <b>Email: icaroribeiro@hotmail.com</b>
+      <br></br>
       <b>LinkedIn: https://www.linkedin.com/in/icaroribeiro</b>
     </div>
   );
